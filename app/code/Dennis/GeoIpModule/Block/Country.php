0de85f24c890ed30cdf36229372fca58d81cd02d
Dennis Keith Gaso <dennis@craftxlogic.com>
2022-11-04 17:23:07 +0800

    TICKET_ID-EX-01 Redirect
    Redirect if country is Russia or China.

--- app/code/Dennis/GeoIpModule/Block/Country.php
+++ app/code/Dennis/GeoIpModule/Block/Country.php
@@ -3,20 +3,32 @@
 namespace Dennis\GeoIpModule\Block;
 
 use Dennis\GeoIpModule\Model\ResourceModel\Item\Collection;
+use Magento\Framework\App\Response\Http;
+use Magento\Framework\App\Response\RedirectInterface;
 use Magento\Framework\DataObject;
+use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\Template;
 
 class Country extends Template
 {
     private $collection;
     private $ipAddress;
+    private $urlInterface;
+    protected $redirect;
+    protected $response;
 
     public function __construct(
         Template\Context $context,
         Collection $collection,
+        UrlInterface $urlInterface,
+        Http $response,
+        RedirectInterface $redirect,
         array $data = []
     ) {
         $this->collection = $collection;
+        $this->urlInterface = $urlInterface;
+        $this->redirect = $redirect;
+        $this->response = $response;
         parent::__construct($context, $data);
     }
 
@@ -45,6 +57,17 @@ class Country extends Template
     }
 
     /**
+     * Get host url
+     * @param $url
+     * @return string
+     */
+
+    public function getCurrentUrl()
+    {
+        return $this->urlInterface->getCurrentUrl();
+    }
+
+    /**
      * The User GeoIP
      * @return DataObject
      */
@@ -63,7 +86,19 @@ class Country extends Template
         if ($this->getTemplate()) {
             return parent::_toHtml();
         }
+
         $country_code = $this->getGeoIp()->getCountryCode();
+        if ($country_code === 'RU' || $country_code === 'CH') {
+            $current_url = $this->getCurrentUrl();
+            $error_page = $current_url . '404';
+
+            // If url is already in error page, don't redirect anymore.
+            if (str_contains($current_url, '404')) {
+                return;
+            }
+            $this->redirect->redirect($this->response, $error_page);
+            return;
+        }
         if ($country_code === 'US') {
             return '<li>' . $country_code . '</li>';
         }
